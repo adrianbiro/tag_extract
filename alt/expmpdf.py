daafbca7b173ff3bcbe538c863f24d242eb60360
Adrián Bíro <biroadrian@protonmail.com>
2022-10-28 13:25:07 +0200

    small fixes

--- alt/expmpdf.py
+++ alt/expmpdf.py
@@ -10,13 +10,16 @@ def openfile(path_to_pdf_file):
         for page_number, page in enumerate(document):
             words = page.get_text("XT-(.*)")
             words_dict[page_number] = words
-            final = fitz.TextPage.extractDICT(words_dict)
-    print(json.dumps(final))
+    print(json.dumps(words_dict))
+        #for page in document:
+        #    wlist = page.get_text_words()
+        #    #wlist = page.get_text() # da text
+        #    #wlist = TextPage.extractDICT() # da text
+        #    print(wlist)
 
 
 def main():
     if len(sys.argv) > 1:
-        my_file = sys.argv[1]
         openfile(Path(sys.argv[1]))
     else:
         print(f"Usage:\v python3 {sys.argv[0]} <file.pdf>")
