6296e2a7bdbeb121594cb899e648e48b61ebb849
Adrián Bíro <biroadrian@protonmail.com>
2022-10-30 09:02:36 +0100

    modular regex

--- expmpdf.py
+++ expmpdf.py
@@ -2,22 +2,41 @@ import sys
 from pathlib import Path
 import json
 import fitz
-#import pyjq  #TODO nechce ho nainštalovať
+import re
 
 def openfile(path_to_pdf_file):
+    regex = "XT-(.*)"
     with fitz.open(path_to_pdf_file) as document:
-        for page in document:
-            wlist = page.get_text("json")
-            #selected_jason = pyqj.first('.blocks[].lines[].spans[] | select(.text |test("XT-(.*)"))', wlist)
-            #print(selected_jason)
-            print(wlist)
-
+        final_list = []
+        for pnum, page in enumerate(document):
+            #wlist = page.get_text("json")
+            full_dict = page.get_text("dict")
+            page_num = {"page_num": pnum}
+            width = {"width": full_dict["width"]}
+            height= {"height": full_dict["height"]}
+            for b in full_dict["blocks"]:
+                if "lines" in b:
+                    for l in b["lines"]:
+                        for s in l["spans"]:
+                            if re.match(regex, s["text"]):
+                                text = {"text": s["text"]}
+                                coor = {
+                                    "x":s["origin"][0],
+                                    "y":s["origin"][1]}
+                                final = [
+                                    page_num,
+                                    width,
+                                    height,
+                                    text,
+                                    coor]
+                                final_list.append(final)
+    return json.dumps(final_list)
 
 def main():
     if len(sys.argv) > 1:
-        openfile(Path(sys.argv[1]))
+        print(openfile(Path(sys.argv[1])))
     else:
-        print(f"Usage:\v python3 {sys.argv[0]} <file.pdf> | jq '.blocks[].lines[].spans[] | select(.text |test(\"XT-(.*)\"))'")
+        print(f"Usage:\vpython3 {sys.argv[0]} <file.pdf>")
         exit(2)
 
 main()
